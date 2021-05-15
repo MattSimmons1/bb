1fd41d833acdb97128732d7d8736f8ae8b103e82
Matt Simmons <matt.simmons@datasparq.co.uk>
2021-05-15 16:01:29 +0100

    add all comment types to injection mode

--- parser/parse-injection.go
+++ parser/parse-injection.go
@@ -46,7 +46,6 @@ func (l *lexer) injectionModeRun() {
 ```bb ... ```
 {-bb ... -}
 
-
 Keep going until we find one of these
 */
 func lexInjection(l *lexer) stateFn {
@@ -58,42 +57,39 @@ func lexInjection(l *lexer) stateFn {
     l.ignore()
     l.emit(itemEOF)
     return nil
-  case r == '/':
-      if len(l.input[l.pos:]) > 2 {
-        if l.input[l.pos:l.pos+3] == "/bb" {
-          l.pos += 3
-          l.ignore()
-          return lexInlineInjection
-        } else if l.input[l.pos:l.pos+3] == "*bb" {
-          l.pos += 3
-          l.ignore()
-          return lexMultilineInjection
+  case r == '/' || r == '\'' || r == '"' || r == '`' || r == '{' || r == '<' || r == '#' || r == '-':
+
+
+      prefix := string(r) + (l.input + "     ")[l.pos:l.pos + 5]  // take the next 6 characters - pad the input so I don't have to check it's long enough to slice
+      log("comment prefix is: " + prefix)
+
+      if prefix == "<!--bb" { // look for multiline with length 6 prefix
+        l.pos += 5
+        l.ignore()
+        return l.scanMultilineInjection("-->")
+      } else if prefix = prefix[:5]; prefix == "\"\"\"bb" || prefix == "'''bb" || prefix == "```bb" { // look for multiline with length 5 prefix
+        l.pos += 4
+        l.ignore()
+        return l.scanMultilineInjection(prefix[:3])
+      } else if prefix = prefix[:4]; prefix == "/*bb" || prefix == "{-bb" { // look for multiline with length 4 prefix
+        l.pos += 3
+        l.ignore()
+        if prefix == "/*bb" {
+          return l.scanMultilineInjection("*/")
+        } else {
+          return l.scanMultilineInjection("-}")
         }
-      }
-
-      return lexInjection
-
-  case r == '-':
-    if len(l.input[l.pos:]) > 2 {
-      if l.input[l.pos:l.pos+3] == "-bb" {
+      } else if prefix == "//bb" || prefix == "--bb" {  // look for inline with length 4 prefix
         l.pos += 3
         l.ignore()
         return lexInlineInjection
-      }
-    }
-
-    return lexInjection
-
-  case r == '#':
-    if len(l.input[l.pos:]) > 1 {
-      if l.input[l.pos:l.pos+2] == "bb" {
+      } else if prefix = prefix[:3]; prefix == "#bb" { // look for inline with length 3 prefix
         l.pos += 2
         l.ignore()
         return lexInlineInjection
+      } else {
+        return lexInjection  // it's not an injection
       }
-    }
-
-    return lexInjection
 
   default:
     return lexInjection
@@ -113,18 +109,33 @@ func lexInlineInjection(l *lexer) stateFn {
   }
 }
 
-
-// injected bb that can only be on one line
-func lexMultilineInjection(l *lexer) stateFn {
-  log("lexMultilineInjection")
+// injected bb that can be on multiple lines and must end with a specific suffix
+func (l *lexer) scanMultilineInjection(suffix string) stateFn {
+  log("scanMultilineInjection")
+  log("suffix is: " + suffix)
+  log("looking for: " + string(rune(suffix[0])))
   for {
-    if r := l.next(); r == '*' || r == eof {
-      if l.next() == '/' {
-        l.pos -= 2
-        l.emit(itemString)
-        l.pos += 2
-        return lexInjection
+    if r := l.next(); r == rune(suffix[0]) {
+
+      for _, s := range suffix[1:] {
+        log(string(rune(s)))
+        if l.next() == rune(s) {
+          log("is in the suffix")
+          // absorb
+        } else {
+          log("is not part of the suffix")
+          break
+        }
       }
+
+      // if we complete the loop for the whole suffix:
+     l.pos -= Pos(len(suffix))  // backtrack so that the suffix isn't ignored
+     l.emit(itemString)
+     l.pos += Pos(len(suffix))
+     return lexInjection
+    } else if r == eof {
+      log("Found EOF before comment suffix - fail silently")
+      return lexInjection  // unclosed comment - fail silently
     } else {
       log(string(l.peek()))
     }
