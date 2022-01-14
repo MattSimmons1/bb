dba85b3846ecd60b0dc938d08f62b9a4ea878b93
Matt Simmons <matthew.simmons.0001@gmail.com>
2022-01-14 16:53:03 +0000

    update python setup file

--- client/python/setup.py
+++ client/python/setup.py
@@ -1,13 +1,27 @@
 
 from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (here / "README.md").read_text()
 
 setup(
     name="bb",
     version="0.1.0",
     description="bb Python Client",
+    long_description=README,
     packages=["bb"],
     author='Matt Simmons',
     author_email='',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
+    ],
     url="https://mattsimmons1.github.io/bb",
     install_requires=[],
     package_data={"bb": ["bb"]},
