922daa185e7a4f7f9baaf6442faf148cf06243f7
Matt Simmons <matthew.simmons.0001@gmail.com>
2023-11-14 20:43:27 +0000

    fix/upgrade wasm

--- playground/README.md
+++ playground/README.md
@@ -8,3 +8,7 @@
 Build the Web Assembly module: 
 
     GOOS=js GOARCH=wasm go build -o playground/bb.wasm ./wasm.go
+
+Remember to update wasm_exec.js
+
+    cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./js/wasm_exec.js
--- wasm.go
+++ wasm.go
@@ -1,37 +1,36 @@
+//go:build js && wasm
 // +build js,wasm
 
 package main
 
 import (
-  "bb/parser"
-  "fmt"
-  "syscall/js"
+	"fmt"
+	"github.com/MattSimmons1/bb/parser"
+	"syscall/js"
 )
 
 func main() {
-  fmt.Println("Hello wasm")
-  js.Global().Get("wasm").Set("bb", js.FuncOf(WASMConvert))
-  js.Global().Get("wasm").Set("bbSyntax", js.FuncOf(WASMSyntax))
+	fmt.Println("Hello wasm")
+	js.Global().Get("wasm").Set("bb", js.FuncOf(WASMConvert))
+	js.Global().Get("wasm").Set("bbSyntax", js.FuncOf(WASMSyntax))
 
-  parser.DefineBuiltInTypes()
+	//parser.DefineBuiltInTypes()
 
-  select {}  // don't exit
+	select {} // don't exit
 }
-
 
 func WASMConvert(this js.Value, p []js.Value) interface{} {
-  fmt.Println(p[0].String())
+	fmt.Println(p[0].String())
 
-  data := parser.Parse(p[0].String())
+	data := parser.Parse(p[0].String())
 
-  return js.ValueOf(data)
+	return js.ValueOf(data)
 }
 
-
 func WASMSyntax(this js.Value, p []js.Value) interface{} {
-  fmt.Println(p[0].String())
+	fmt.Println(p[0].String())
 
-  data := parser.Syntax(p[0].String())
+	data := parser.Syntax(p[0].String())
 
-  return js.ValueOf(data)
+	return js.ValueOf(data)
 }
